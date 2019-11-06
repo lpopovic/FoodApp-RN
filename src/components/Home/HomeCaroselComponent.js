65bf4cf33dc559dc31c8ef4626de2e9bc4b6590d
Lazar Popovic <lazar.popovic.riitech.com>
2019-11-06 11:30:20 +0100

    HomeScreen add Carosel and Category static

--- src/components/Home/HomeCaroselComponent.js
+++ src/components/Home/HomeCaroselComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity } from 'react-native';
-import Carousel , { Pagination } from 'react-native-snap-carousel';
+import Carousel, { Pagination } from 'react-native-snap-carousel'
 import { BASE_COLOR } from '../../styles';
+import { TestAssets } from '../../assets'
 const SLIDER_FIRST_ITEM = 1;
 
 class HomeCaroselComponent extends Component {
-    
+
     constructor(props) {
         super(props)
         this.state = {
@@ -15,12 +16,15 @@ class HomeCaroselComponent extends Component {
 
     _renderItem = ({ item, index }) => {
         return (
-            <View key={index} style={{ alignItems: 'center', alignContent: 'center', justifyContent: 'center', borderRadius: 5 }}>
-                <TouchableOpacity activeOpacity={1} onPress={() => alert("Click carosel index:"+(index-this.props.data.length))}>
-                    <Image style={{ height: Dimensions.get('window').width * 9/16 , width: Dimensions.get('window').width, resizeMode: 'cover' }} source={{ uri: item.image }} />
+            <View key={index} style={{ alignItems: 'center', alignContent: 'center', justifyContent: 'center' }}>
+                <TouchableOpacity activeOpacity={1} onPress={() => alert("Click carosel index:" + (index - 3 ))}>
+                    <Image style={{ height: Dimensions.get('window').width * 9 / 16, width: Dimensions.get('window').width, resizeMode: 'cover' }} source={{ uri: item.image }} />
                 </TouchableOpacity>
             </View>
         );
+    }
+    snapIndexCarosel = (index) => {
+         this.setState({ sliderActiveSlide: index })
     }
 
     render() {
@@ -29,13 +33,15 @@ class HomeCaroselComponent extends Component {
                 <Carousel
                     ref={(c) => { this._carousel = c; }}
                     data={this.props.data}
-                    renderrItem={this._renderItem}
-                    onSnapToItem={(index) => this.setState({ sliderActiveSlide: index }) }
+                    renderItem={this._renderItem}
+                    onSnapToItem={(index) => this.snapIndexCarosel(index)}
                     sliderWidth={Dimensions.get('window').width}
                     itemWidth={Dimensions.get('window').width}
                     firstItem={SLIDER_FIRST_ITEM}
                     autoplay={true}
                     loop={true}
+                    // enableMomentum={true}
+                    // loopClonesPerSide = {this.props.data.length}
                     // enableSnap={true}
                     // activeSlideAlignment="start"
                     animationOptions={{
@@ -48,16 +54,16 @@ class HomeCaroselComponent extends Component {
                     animationFunc={'none'}
                 />
                 <Pagination
-                  dotsLength={this.props.data.length}
-                  activeDotIndex={this.state.sliderActiveSlide}
-                  containerStyle={styles.paginationContainer}
-                  dotColor={BASE_COLOR.blue}
-                  dotStyle={styles.paginationDot}
-                  inactiveDotColor={BASE_COLOR.black}
-                  inactiveDotOpacity={0.4}
-                  inactiveDotScale={0.6}
-                  carouselRef={this._carousel}
-                  tappableDots={!!this._carousel}
+                    dotsLength={this.props.data.length}
+                    activeDotIndex={this.state.sliderActiveSlide}
+                    containerStyle={styles.paginationContainer}
+                    dotColor={BASE_COLOR.blue}
+                    dotStyle={styles.paginationDot}
+                    inactiveDotColor={BASE_COLOR.black}
+                    inactiveDotOpacity={0.4}
+                    inactiveDotScale={0.6}
+                    carouselRef={this._carousel}
+                    tappableDots={!!this._carousel}
                 />
             </View>
         )
--- src/screen/Home/HomeScreen.js
+++ src/screen/Home/HomeScreen.js
@@ -1,5 +1,10 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import {
+    View,
+    ScrollView,
+    RefreshControl,
+    StyleSheet
+} from 'react-native';
 import { ScreenName } from '../../helpers'
 import BaseScreen from "../BaseScreen/BaseScreen"
 import Header from '../../components/common/BaseHeader'
@@ -8,28 +13,31 @@ import { connect } from 'react-redux';
 import { NAV_COLOR, BASE_COLOR } from '../../styles';
 import { CategorySectionList } from '../../components/Category/CategoryList'
 import HomeCaroselComponent from '../../components/Home/HomeCaroselComponent';
+
 class HomeScreen extends BaseScreen {
     static navigationOptions = {
         header: null,
-        // headerVisible: false,
-        // headerBackTitle: "Photo",
+
     };
     constructor(props) {
         super(props)
         this.state = {
+            loading: false,
+            refreshing: false,
             carosel: [
                 {
-                    image: "https://thenypost.files.wordpress.com/2019/09/junk-food-turns-kid-blind.jpg?quality=90&strip=all&w=1033"
-                },
-                {
-                    image: "https://caspiannews.com/media/caspian_news/all_original_photos/1528831479_7183783_1528831390_5761793SFF-Foto-2018-001web2.jpg"
+                    image: "https://images.startups.co.uk/wp-content/uploads/2018/12/20162418/business-ideas-2019-plant-based-foods.jpg"
                 },
                 {
                     image: "http://www.colleges-fenway.org/wp-content/uploads/2018/09/food-festival-1.jpg"
                 },
                 {
+                    image: "https://homepages.cae.wisc.edu/~ece533/images/cat.png"
+                },
+                {
                     image: "https://images.startups.co.uk/wp-content/uploads/2018/12/20162418/business-ideas-2019-plant-based-foods.jpg"
-                }
+                },
+
             ]
         }
     }
@@ -37,6 +45,11 @@ class HomeScreen extends BaseScreen {
     componentDidMount() {
         super.componentDidMount()
         this.setStatusBarStyle(NAV_COLOR.headerBackground, true)
+        this.setNewStateHandler({ loading: true })
+
+        setTimeout(() => {
+            this.setNewStateHandler({ loading: false })
+        }, 1000);
     }
     componentWillUnmount() {
         super.componentWillUnmount()
@@ -48,14 +61,49 @@ class HomeScreen extends BaseScreen {
             onPressSeeMore={() => this.pushNewScreen(ScreenName.PlaceListScreen())}
         />
     )
+    caroselContent = () => (
+        <HomeCaroselComponent data={this.state.carosel} />
+    )
+    _onRefresh = () => {
+        this.setNewStateHandler({ refreshing: true });
+        clearTimeout(this.refreshTimeout)
+        this.refreshTimeout = setTimeout(() => {
+            this.setNewStateHandler({ refreshing: false });
+        }, 1000);
+    }
+    mainContent = () => {
+        const { refreshing } = this.state
+        return (
+            <ScrollView
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={this._onRefresh}
+                        tintColor={BASE_COLOR.blue}
+                        colors={[BASE_COLOR.blue]}
+                    />
+                }
+                style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+                    <View>
+                        {this.caroselContent()}
+                    </View>
+                    <View>
+                        {this.categoryListContent()}
+                    </View>
+
+                </View>
+            </ScrollView>
+        )
+    }
     render() {
-        const mainDisplay = this.categoryListContent()
+        const { loading } = this.state
+        const mainDisplay = loading ? this.activityIndicatorContent(BASE_COLOR.blue) : this.mainContent()
         return (
             <SafeAreaView style={styles.safeAreaHeader}>
                 <View style={styles.mainContainer}>
                     <Header backgroundColor={NAV_COLOR.headerBackground} />
                     {mainDisplay}
-                    <HomeCaroselComponent data={this.state.carosel} />
 
                 </View>
             </SafeAreaView>
