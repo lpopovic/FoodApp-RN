d81870ce04566f3081179bd6ac33a2b113eab056
Lazar Popovic <lazar.popovic.riitech.com>
2019-11-20 15:58:38 +0100

    create HistoryOrderList

---
+++ src/components/HistoryOrder/HistoryOrderList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class HistoryOrderList extends Component {
+
+    render() {
+        return (
+            <View style={styles.mainContainer}>
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1
+    }
+});
+
+
+export default HistoryOrderList;
