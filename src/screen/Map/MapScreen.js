02b29fe64cb43c304590116f6490a2e404101c5c
Lazar Popovic <lazar.popovic.riitech.com>
2019-11-26 16:30:32 +0100

    Carosel item update layout on MapScreen

--- src/screen/Map/MapScreen.js
+++ src/screen/Map/MapScreen.js
@@ -22,6 +22,7 @@ import SegmentedControlTab from "react-native-segmented-control-tab";
 import Carousel from 'react-native-snap-carousel';
 import { PlaceNetwork, ParamsUrl, } from '../../service/api'
 import { TestAssets, IconAssets } from '../../assets'
+import Icon from 'react-native-vector-icons/dist/Ionicons';
 import MapView from 'react-native-maps';
 import { connect } from 'react-redux';
 
@@ -29,6 +30,8 @@ import { connect } from 'react-redux';
 const zoom = 10
 const zoomMarker = 13
 const distanceDelta = Math.exp(Math.log(360) - (zoom * Math.LN2));
+
+
 const latitudeDeltaMarker = Math.exp(Math.log(360) - (zoomMarker * Math.LN2));
 const longitudeDeltaMarker = Dimensions.get('window').width / Dimensions.get('window').height * latitudeDeltaMarker
 // end mapView
@@ -187,6 +190,7 @@ class MapScreen extends BaseScreen {
 
         this.pushNewScreen({ routeName: ScreenName.PlaceDetailScreen(), key: `${Math.random() * 10000}${place._id}`, params: { _id: place._id } })
     }
+
     setNewRegion = (index) => {
 
         clearTimeout(this.regionTimeout);
@@ -196,7 +200,6 @@ class MapScreen extends BaseScreen {
             const { latitudeDelta, longitudeDelta } = region
             if (mapPlaces.length > 0) {
                 const coordinate = mapPlaces[index].coordinate;
-
                 this.map.animateToRegion(
                     {
                         ...coordinate,
@@ -216,8 +219,28 @@ class MapScreen extends BaseScreen {
         const image = item.image.image11t
         const title = item.name
         const rating = item.avgRating
-        const timeDelivery = item.delivery == true ? '45 min.' : 'No delivery'
-        const priceTag = item.avgPriceTag
+        const delivery = item.delivery
+        const timeDelivery = item.estimatedDeliveryTime
+        const priceTag = item.returnAvgPriceTag()
+
+        deliveryContent = (delivery, timeDelivery) => {
+            if (delivery) {
+                return (
+                    <>
+                        <View style={stylesCard.spaceView} />
+                        <View style={stylesCard.itemOtherContainer}>
+                            <Icon name="ios-bicycle" size={16} color={BASE_COLOR.black} />
+                            <Text
+                                style={[stylesCard.baseText, { marginLeft: 4 }]}>
+                                {timeDelivery} min.
+                            </Text>
+                        </View>
+                    </>
+                )
+            }
+        }
+
+
         return (
             <TouchableOpacity key={index} activeOpacity={0.8} onPress={() => this.placeSelectHandler(item)}>
                 <View style={this.cardStyle(index)}>
@@ -242,13 +265,7 @@ class MapScreen extends BaseScreen {
                                     {priceTag}
                                 </Text>
                             </View>
-                            <View style={stylesCard.spaceView} />
-                            <View style={stylesCard.itemOtherContainer}>
-                                <Text
-                                    style={[stylesCard.baseText]}>
-                                    {timeDelivery}
-                                </Text>
-                            </View>
+                            {deliveryContent(delivery, timeDelivery)}
                             <View style={stylesCard.spaceView} />
                             <View style={stylesCard.itemOtherContainer}>
                                 <Image
