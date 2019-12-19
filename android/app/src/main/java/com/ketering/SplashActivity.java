83f16ffa815aa8e3f941b3d35c509ded15400b81
blagoje.vukovic.riitech <user@Users-MacBook-Pro-10.local>
2019-12-19 11:07:31 +0100

    splash screen bug fixed

--- android/app/src/main/java/com/ketering/SplashActivity.java
+++ android/app/src/main/java/com/ketering/SplashActivity.java
@@ -11,6 +11,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
         startActivity(intent);
         finish();
     }
