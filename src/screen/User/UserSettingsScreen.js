d91e49c094218c9db380dd91000e2080619a080a
Lazar Popovic <lazar.popovic.riitech.com>
2019-12-24 15:29:15 +0100

    UserSettingsScreen base import

--- src/screen/User/UserSettingsScreen.js
+++ src/screen/User/UserSettingsScreen.js
@@ -1,21 +1,84 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+} from 'react-native';
+import BaseScreen from '../BaseScreen/BaseScreen';
+import Header from '../../components/common/BtnHeader'
+import {
+    NAV_COLOR,
+    BASE_COLOR,
+} from '../../styles'
 
-class UserSettingsScreen extends Component {
 
-    render() {
+class UserSettingsScreen extends BaseScreen {
+    static navigationOptions = {
+        header: null,
+    };
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        super.componentDidMount()
+        this.setStatusBarStyle(NAV_COLOR.headerBackground, true)
+        setTimeout(() => {
+            this.setNewStateHandler({
+                loading: false
+            })
+        }, 500);
+
+    }
+    componentWillUnmount() {
+        super.componentWillUnmount()
+    }
+    onSaveBtnPress = () => {
+
+    }
+
+    mainContent = () => {
         return (
-            <View style={styles.mainContainer}>
+            <ScrollView
+                bounces={false}>
 
-            </View>
+            </ScrollView>
+        )
+    }
+    render() {
+        const { loading } = this.state
+        const mainDisplay = loading ? this.activityIndicatorContent(BASE_COLOR.blue) : this.mainContent()
+        return (
+            <SafeAreaView style={styles.safeAreaHeader}>
+                <View style={styles.mainContainer}>
+                    <Header
+                        tintColor={BASE_COLOR.darkGray}
+                        title={'Back'}
+                        btnTitle={'Save'}
+                        btnPress={() => this.onSaveBtnPress()}
+                        backgroundColor={NAV_COLOR.headerBackground} />
+                    {mainDisplay}
+                </View>
+            </SafeAreaView>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    safeAreaHeader: {
+        backgroundColor: NAV_COLOR.headerBackground,
+        flex: 1,
+    },
     mainContainer: {
-        flex: 1
-    }
+        flex: 1,
+        backgroundColor: BASE_COLOR.white
+    },
 });
 
 
