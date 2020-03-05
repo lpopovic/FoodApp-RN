dc318e6c45e72cf78bbfad53b8510f1d5c80cbe1
Lazar Popovic <lazar.popovic.riitech.com>
2020-03-05 12:14:12 +0100

    App.js set default setup for system font and scroll indicator

--- App.js
+++ App.js
@@ -7,12 +7,27 @@
  */
 
 import React, { Component } from 'react';
-
+import {
+Text,
+TextInput,
+ScrollView,
+} from 'react-native'
 import Navigator  from './src/components/TabNavigation/Navigator'
 
 import { Provider } from 'react-redux'
 import configurateStore from './src/store/configureStore'
+import { BASE_COLOR } from './src/styles';
 const store = configurateStore();
+
+if (Text.defaultProps == null) Text.defaultProps = {};
+Text.defaultProps.allowFontScaling = false;
+
+if (TextInput.defaultProps == null) TextInput.defaultProps = {};
+TextInput.defaultProps.placeholderTextColor = BASE_COLOR.gray
+
+if (ScrollView.defaultProps == null) ScrollView.defaultProps = {};
+ScrollView.defaultProps.scrollIndicatorInsets = { right: 1 };
+
 export default class App extends Component {
 
   render() {
